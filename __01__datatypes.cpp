84ee9afe2487f4ae0367517a941d38966d8e0a82
MohtasinLipu <hossainmdfaruk131@gmail.com>
2026-07-21 22:01:51 +0600

    01

---
+++ __01__datatypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(){
+//int,char,double(no float),boolina egula same syntax and same vabe kaj kore
+
+
+//string
+std::string name="Mohtasin Bin Farik Lipu";
+std::cout << name << '\n';
+return 0;
+
+
+
+
+
+
+}
